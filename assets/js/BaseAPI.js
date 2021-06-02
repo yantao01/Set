14ef6f64ba1bc1c9015afb9f19cb4d84d0e85670
yantao <yantao01@zx.cn>
2021-06-02 23:08:29 +0800

    添加了页面渲染

--- assets/js/BaseAPI.js
+++ assets/js/BaseAPI.js
@@ -3,6 +3,34 @@
 //在这个函数中，可以拿到我们给的Ajax提供的配置对象
 $.ajaxPrefilter(function(options) {
     //在发起真正的Ajax请求之前，统一拼接请求的根路径
-    options.url = 'http://ajax.frontend.itheima.net' + options.url;
-    console.log(options.url);
+    // options.url = 'http://ajax.frontend.itheima.net'+ options.url
+
+
+    // 统一为有权限的接口，设置headers请求头
+    if(options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            // 请求头配置对象headers
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+
+
+    //全局统一挂载complete回调函数
+
+    //不论成功或者是失败，最终都会调用这个complete 回调函数
+    options.complete = function(res) {
+        // console.log(res);
+        // console.log('执行了complete回调函数');
+        //在complete回调函数中，可以使用res.responseJSON拿到服务器响应回来的数据
+        if(res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            
+            //强制清空token
+            localStorage.removeItem('token');
+           
+            //强制跳转到login页面
+            location.href = '/login.html'
+        }
+    }
+    
 })
